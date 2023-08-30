bace74caf63509b0688ffd4042ccb3722d82fb2f
Waffle Beast <gambitbrian7m@gmail.com>
2023-08-30 15:56:49 +0300

    [FEAT] some changes in navbar

--- front-end/src/components/NavBar/index.tsx
+++ front-end/src/components/NavBar/index.tsx
@@ -14,7 +14,18 @@ const NavBar: React.FC = () => {
         </div>
 
         <div className={styles.nav__buttons}>
-          {isAuth && <button className={styles.add}>Add Post</button>}
+          {isAuth && (
+            <button className={styles.add}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="25"
+                viewBox="0 -960 960 960"
+                width="27"
+                fill="green">
+                <path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z" />
+              </svg>
+            </button>
+          )}
 
           <div className={styles.login}>
             {isAuth ? (
