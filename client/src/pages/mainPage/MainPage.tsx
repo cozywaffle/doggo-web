0bd8d5f51639801fc3fec7dc5197c7ce069e78af
Waffle Beast <gambitbrian7m@gmail.com>
2023-09-12 21:12:17 +0300

    [FEAT](client) header

---
+++ client/src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import Header from "../../components/header/Header";
+
+const MainPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>SECTION</h1>
+      </section>
+    </>
+  );
+};
+
+export default MainPage;
